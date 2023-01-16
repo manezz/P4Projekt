52ed25ac5f876eb3d3df2d25d4449fcbc831ddcb
Bjarkebek <bjarke44@gmail.com>
2023-01-16 14:51:54 +0100

    optimized

--- Social medie projekt/Client/src/app/app.component.ts
+++ Social medie projekt/Client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
-  styleUrls: []
+  styles: []
 })
 export class AppComponent {
   title = 'Client';
