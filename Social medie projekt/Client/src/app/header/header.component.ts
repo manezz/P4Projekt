0af368f58617da5b5ac4023eac2816d6d02bf46d
LeonardF-96 <79829934+LeonardF-96@users.noreply.github.com>
2023-01-17 08:19:08 +0100

    header commit 2

--- Social medie projekt/Client/src/app/header/header.component.ts
+++ Social medie projekt/Client/src/app/header/header.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `
+    <nav>
+      <div class="topBar">
+        <div class="left">
+          <img src="/assets/images/socialmachine.png">
+          <a href="#">Home</a>
+        </div>
+        <div class="right">
+          <a href="#">Log In</a>
+        </div>
+      </div>
+    </nav>`,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
