7ece6a1ed1f88057103cc9aac48670125c031c1c
Wiktorxdd <wiktorskalskiw@gmail.com>
2023-01-18 13:03:44 +0100

    model fixd

--- Social_medie_projekt/Client/src/app/_models/user.ts
+++ Social_medie_projekt/Client/src/app/_models/user.ts
@@ -1,8 +1,11 @@
+import { Login } from "./login";
+
 export interface User {
     userId: number;
-    username: string;
-    profilePicURL: string;
-    bio: string;
-    followers?: Array<string>;
-    following?: Array<string>;
+    loginId: number;
+    firstName: string;
+    lastName: string;
+    address: string;
+    created: Date;
+    login: Login;
 }
