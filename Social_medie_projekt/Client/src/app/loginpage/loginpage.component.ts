587518243df01ae22174243d4f74f7baad600e00
LeonardF-96 <79829934+LeonardF-96@users.noreply.github.com>
2023-01-18 14:57:15 +0100

    commit to leo

--- Social_medie_projekt/Client/src/app/loginpage/loginpage.component.ts
+++ Social_medie_projekt/Client/src/app/loginpage/loginpage.component.ts
@@ -3,28 +3,28 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loginpage',
-  // standalone: true,
-  // imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule],
   template: `
     <div class="formControl">
       <img src="/assets/images/socialmachine.png" width="600">
       <div class="formDiv">
-        <label>Email</label>
-        <input type="text"/>
+        <label for="email">Email</label>
+        <input type="text" id="email"/>
       </div>
       <div class="formDiv">
-        <label>Password</label>
-        <input type="password"/>
+        <label for="password">Password</label>
+        <input type="password" id="password"/>
       <!-- </div>
         <span class="error"*ngIf="userForm.get('userName')?.invalid && userForm.get('passHash')?.touched">Fill out form!</span>
       </div> -->
       <div class="buttonDiv">
         <button>Login</button>
-        <button class="right" routerLink="/login-create">Create new user</button>
+        <button class="right">Create new user</button>
       </div>
     </div>`,
   styles: [`.formControl {display:flex; height: 700px; justify-content: center; align-items: center; flex-direction: column;}`]
 })
 export class LoginpageComponent {
-
 }
+
