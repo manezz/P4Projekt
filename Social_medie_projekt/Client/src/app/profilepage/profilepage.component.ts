3ab462a12082f27684fe2de5c151f9af3c529f20
Bjarkebek <bjarke44@gmail.com>
2023-01-19 13:00:51 +0100

    began work on profilepage

--- Social_medie_projekt/Client/src/app/profilepage/profilepage.component.ts
+++ Social_medie_projekt/Client/src/app/profilepage/profilepage.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../_services/data.service';
+import { PostService } from '../_services/post.service';
 
 @Component({
   selector: 'app-profilepage',
   // standalone: true,
   // imports: [CommonModule],
   template: `
-    <h1>
-      Profilepage works!
-    </h1>
+    <div id="post" *ngFor="let data of post">
+    <!-- <h1 id="title">{{data.title}}</h1>
+    <h5 id="username">{{data.username}}</h5>
+    <h3 id="description">{{data.desc}}</h3>
+    <p id="date">{{data.date}}</p>
+    <p id="tags">{{data.tag}} </p> -->
+    <button class="postBtn" id="like"><3</button>
+  </div>
   `,
-  styles: [`
-  h1{
-    color: white;
-  }
-  `]
+  styleUrls: ["../_css/poststyle.css"]
 })
 export class ProfilepageComponent {
 
+  post: any
+  
+  // sætter values i getTempData til data
+  constructor(post:PostService){
+    this.post = post.getAll()
+  }
 }
