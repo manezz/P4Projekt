2bbf103b8eecbdd72a13e805be15db8da14c0721
Bjarkebek <bjarke44@gmail.com>
2023-01-20 10:02:17 +0100

    added user and tempdata service

---
+++ Social_medie_projekt/Client/src/app/_services/user.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+    private readonly apiUrl = environment.apiUrl + 'User';
+
+    constructor(private http: HttpClient) { }
+
+    getAllUsers(): Observable <User[]>{
+        return this.http.get<User[]>(this.apiUrl)
+    }
+    
+    getUserById(userId: number): Observable<User> {
+        return this.http.get<User>(`${this.apiUrl}/${userId}`)
+    }
+
+    createUser(user: User): Observable<User>{
+        return this.http.post<User>(this.apiUrl, user)
+    }
+
+    updateUser(Id:number,user:User): Observable<User>{
+        return this.http.put<User>(`${this.apiUrl}/${Id}`,user)
+    }
+
+}
