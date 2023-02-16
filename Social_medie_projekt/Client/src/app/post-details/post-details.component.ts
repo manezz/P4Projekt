a662f71960a06af6ad141af6db82eb3bac10e5be
Bjarkebek <bjarke44@gmail.com>
2023-02-16 14:38:17 +0100

    modified imports

--- Social_medie_projekt/Client/src/app/post-details/post-details.component.ts
+++ Social_medie_projekt/Client/src/app/post-details/post-details.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { ActivatedRoute } from '@angular/router';
-import { User } from '../_models/user';
 import { PostService } from '../_services/post.service';
 import { Post } from '../_models/post';
 
@@ -44,8 +43,8 @@ export class PostDetailsComponent {
 
   constructor(private route: ActivatedRoute, private authService: AuthService, private postService: PostService){ }
 
-  ngOnInit(): void {
-    this.authService.currentUser.subscribe(x => this.currentUser = x)
+  ngOnInit(): void  {
+    this.authService.currentUser.subscribe(x => this.currentUser = x)      
     this.route.params.subscribe(params => { this.postService.GetPostByPostId(params['postId']).subscribe(x => this.post = x) })
   }
 
