205a1dff77d9e41e43327413b22c79d041cc0570
Bjarkebek <bjarke44@gmail.com>
2023-03-03 13:45:18 +0100

    small fixes

--- Social_medie_projekt/Client/src/app/_services/post.service.ts
+++ Social_medie_projekt/Client/src/app/_services/post.service.ts
@@ -1,23 +1,22 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Post } from '../_models/post'
-import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  private readonly apiUrl = environment.apiUrl + 'Post'
+  private readonly apiUrl = environment.apiUrl + 'post'
 
   constructor(private http: HttpClient) { }
 
 
   // henter ALLE posts
   getAll(): Observable<Post[]>{
-    return this.http.get<Post[]>(`${this.apiUrl}`)
+    return this.http.get<Post[]>(this.apiUrl)
   }
 
   // henter en bestemt post
