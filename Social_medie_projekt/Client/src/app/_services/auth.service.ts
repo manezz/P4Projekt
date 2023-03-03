8b0b49f297cabb1d5749a62287a07bdc0bae9820
Bjarkebek <bjarke44@gmail.com>
2023-03-03 08:07:01 +0100

    changed 'signinresponse' to 'Login'

--- Social_medie_projekt/Client/src/app/_services/auth.service.ts
+++ Social_medie_projekt/Client/src/app/_services/auth.service.ts
@@ -1,57 +1,61 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, first, last, map, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Role } from '../_models/role';
-import { User } from '../_models/user';
-import { Login } from '../_models/login';
-import { EmailValidator } from '@angular/forms';
-import { SignInResponse } from '../Req-Res/signInResponse';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject, first, last, map, Observable } from 'rxjs'
+import { environment } from 'src/environments/environment'
+import { Role } from '../_models/role'
+import { User } from '../_models/user'
+import { Login } from '../_models/login'
+import { EmailValidator } from '@angular/forms'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject: BehaviorSubject<SignInResponse>;
-  currentUser: Observable<SignInResponse>;
+  private currentUserSubject?: BehaviorSubject<Login>
+  currentUser: Observable<Login>
 
   constructor(private http: HttpClient) { 
-    this.currentUserSubject = new BehaviorSubject<SignInResponse>(
+    this.currentUserSubject = new BehaviorSubject<Login>(
       JSON.parse(sessionStorage.getItem('currentUser') as string)
-    );
-    this.currentUser = this.currentUserSubject.asObservable();
+    )
+    this.currentUser = this.currentUserSubject.asObservable()
   }
 
-  public get CurrentUserValue(): SignInResponse {
-    return this.currentUserSubject.value;
+  public get CurrentUserValue(): Login {
+    return this.currentUserSubject!.value
   }
 
+
+
+
+  
   public getUser(): any {
-    const user = window.sessionStorage.getItem('currentUser');
+    const user = window.sessionStorage.getItem('currentUser')
     if (user) {
-      return JSON.parse(user);
+      return JSON.parse(user)
     }
-    return {};
+    return {}
   }
+
 
   login(email: string, password: string){
-    let authenticateUrl = `${environment.apiUrl}Login/authenticate`;
-    return this.http.post<SignInResponse>(authenticateUrl, { "email": email, "password": password}).pipe(map(user => {
-      sessionStorage.setItem('currentUser', JSON.stringify(user));
-
-      this.currentUserSubject.next(user as SignInResponse);
-      return user;
+    let authenticateUrl = `${environment.apiUrl}Login/authenticate`
+    return this.http.post<Login>(authenticateUrl, { "email": email, "password": password}).pipe(map(user => {
+      sessionStorage.setItem('currentUser', JSON.stringify(user))
+      this.currentUserSubject?.next(user as Login)
+      console.log(user)
+      // already returns user so no return needed down here
     }))
   }
 
   logout(){
     sessionStorage.removeItem('currentUser');
-    this.currentUserSubject = new BehaviorSubject<SignInResponse>(JSON.parse(sessionStorage.getItem('currentUser') as string));
-    this.currentUser = this.currentUserSubject.asObservable();
+    this.currentUserSubject   = new BehaviorSubject<Login>(JSON.parse(sessionStorage.getItem('currentUser') as string))
+    this.currentUser          = this.currentUserSubject.asObservable()
   }
 
   register(login: Login): Observable<Login> {
-    let registerUrl = `${environment.apiUrl}login/register/`;
+    let registerUrl = `${environment.apiUrl}login/register/`
     return this.http.post<Login>(registerUrl, login)
   }
 }
