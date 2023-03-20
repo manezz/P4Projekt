4d879349d4c88e2565e61391689eefed3f444dd4
Bjarkebek <bjarke44@gmail.com>
2023-03-20 09:22:52 +0100

    fixed posts and post css

--- Social_medie_projekt/Client2/src/app/indexpage/indexpage.component.ts
+++ Social_medie_projekt/Client2/src/app/indexpage/indexpage.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router} from '@angular/router';
 import { PostService } from '../_services/post.service';
+import { AuthService } from '../_services/auth.service';
 import { Post } from '../_models/post';
 import { Tag } from '../_models/tags';
 import { CreatePostPageComponent } from '../create-postpage/create-postpage.component';
@@ -15,14 +16,31 @@ import { LikeComponent } from '../components/like.component';
   styleUrls: ['indexpage.component.css'],
 })
 export class IndexpageComponent {
+  currentUser: any = {}
   posts: Post[] = [];
   tags: Tag[] = [];
 
-  constructor(private postService: PostService) {
+  constructor(
+    private postService: PostService,
+    private authService: AuthService,
+    private router: Router
+    ){
     this.tags = [];
   }
 
   ngOnInit(): void {
-    this.postService.getAll().subscribe((p) => (this.posts = p));
+    this.authService.currentUser.subscribe(x => this.currentUser = x )
+    this.postService.getAll().subscribe(p => this.posts = p)
+  }
+
+  postLink(user: any) {
+    if(user.userId == this.currentUser.loginId){
+      // linker til brugerens egen profilside
+      this.router.navigateByUrl('/profile')
+    }
+    else{
+      // linker til en andens bruger profilside
+      this.router.navigate(['/profile/', user.userId])
+    }
   }
 }
