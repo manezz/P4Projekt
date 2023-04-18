5023958e3cf78bf92f5da43ed4623ad84170c636
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-04-18 14:55:32 +0200

    working on profilepage

--- Social_medie_projekt/Client2/src/app/pages/profilepage/profilepage.component.ts
+++ Social_medie_projekt/Client2/src/app/pages/profilepage/profilepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Post } from '../../_models/post';
 import { PostService } from '../../_services/post.service';
@@ -9,6 +9,7 @@ import { CreatePostPageComponent } from '../create-postpage/create-postpage.comp
 import { PostComponent } from '../post/post.component';
 import { ProfilepageSidenavComponent } from '../profilepage-sidenav/profilepage-sidenav.component';
 import { ProfilepageCenternavComponent } from 'src/app/pages/profilepage-centernav/profilepage-centernav.component';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-profilepage',
@@ -31,14 +32,20 @@ export class ProfilepageComponent implements OnInit {
 
   constructor(
     private postService: PostService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe((x) => (this.currentUser = x));
-    this.postService
-      .GetPostByUserId(this.currentUser.user.userId)
-      .subscribe((x) => (this.posts = x));
+    if (this.router.url === '/profile') {
+      this.authService.currentUser.subscribe((x) => (this.currentUser = x));
+      this.postService
+        .GetPostByUserId(this.currentUser.user.userId)
+        .subscribe((x) => (this.posts = x));
+    } else {
+    }
 
     this.screenWidth = window.innerWidth;
   }
