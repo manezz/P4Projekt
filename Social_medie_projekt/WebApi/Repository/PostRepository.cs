37b7dbbfc43e1b2305f16bd4f8c6464aca49497e
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-05-10 13:02:46 +0200

    added PostTagRepository

--- Social_medie_projekt/WebApi/Repository/PostRepository.cs
+++ Social_medie_projekt/WebApi/Repository/PostRepository.cs
@@ -13,12 +13,6 @@
 
         // POST UPDATE LIKES
         Task<Post?> UpdatePostLikesByIdAsync(int id, int like);
-
-        // POSTTAGS
-        Task<List<PostTag>> GetPostTagsByPostId(int postId);
-        Task<PostTag> CreatePostTagAsync(PostTag newPostsTag);
-        Task<PostTag> DeletePostTagAsync(PostTag newPostsTag);
-        Task<PostTag> UpdatePostTagAsync(PostTag newPostsTag);
     }
 
     public class PostRepository : IPostRepository
@@ -108,49 +102,6 @@
             }
 
             return post;
-        }
-
-        public async Task<List<PostTag>> GetPostTagsByPostId(int postId)
-        {
-            return await _context.PostTag
-                .Include(p => p.Post)
-                .Include(t => t.Tag)
-                .Where(p => p.PostId == postId)
-                .Select(p => p)
-                .ToListAsync();
-        }
-
-        public async Task<PostTag> CreatePostTagAsync(PostTag postTag)
-        {
-            _context.PostTag.Add(postTag);
-            await _context.SaveChangesAsync();
-            return postTag;
-        }
-
-        public async Task<PostTag> UpdatePostTagAsync(PostTag postsTag)
-        {
-            var postag2 = from posttag in _context.PostTag
-                          where posttag.PostId == postsTag.PostId
-                          where posttag.TagId != postsTag.TagId
-                          select posttag;
-
-            var postag = await _context.PostTag
-                .Where(x => x.PostId == postsTag.PostId)
-                .Where(x => x.TagId == postsTag.TagId)
-                .Select(x => x)
-                .ToListAsync();
-
-            _context.PostTag.Add(postsTag);
-            await _context.SaveChangesAsync();
-            return postsTag;
-        }
-
-        public async Task<PostTag> DeletePostTagAsync(PostTag postsTag)
-        {
-            _context.Remove(postsTag);
-            _context.PostTag.Remove(postsTag);
-            await _context.SaveChangesAsync();
-            return postsTag;
         }
     }
 }
---
+++ Social_medie_projekt/WebApi/Repository/PostTagRepository.cs
@@ -0,0 +1,63 @@
+﻿namespace WebApi.Repository
+{
+    public interface IPostTagRepository
+    {
+        Task<List<PostTag>> GetPostTagsByPostId(int postId);
+        Task<PostTag> CreatePostTagAsync(PostTag newPostsTag);
+        Task<PostTag> DeletePostTagAsync(PostTag newPostsTag);
+        Task<PostTag> UpdatePostTagAsync(PostTag newPostsTag);
+    }
+
+    public class PostTagRepository : IPostTagRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public PostTagRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PostTag>> GetPostTagsByPostId(int postId)
+        {
+            return await _context.PostTag
+                .Include(p => p.Post)
+                .Include(t => t.Tag)
+                .Where(p => p.PostId == postId)
+                .Select(p => p)
+                .ToListAsync();
+        }
+
+        public async Task<PostTag> CreatePostTagAsync(PostTag postTag)
+        {
+            _context.PostTag.Add(postTag);
+            await _context.SaveChangesAsync();
+            return postTag;
+        }
+
+        public async Task<PostTag> UpdatePostTagAsync(PostTag postsTag)
+        {
+            var postag2 = from posttag in _context.PostTag
+                          where posttag.PostId == postsTag.PostId
+                          where posttag.TagId != postsTag.TagId
+                          select posttag;
+
+            var postag = await _context.PostTag
+                .Where(x => x.PostId == postsTag.PostId)
+                .Where(x => x.TagId == postsTag.TagId)
+                .Select(x => x)
+            .ToListAsync();
+
+            _context.PostTag.Add(postsTag);
+            await _context.SaveChangesAsync();
+            return postsTag;
+        }
+
+        public async Task<PostTag> DeletePostTagAsync(PostTag postsTag)
+        {
+            _context.Remove(postsTag);
+            _context.PostTag.Remove(postsTag);
+            await _context.SaveChangesAsync();
+            return postsTag;
+        }
+    }
+}
