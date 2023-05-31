570c3d6aa8dfb07c24bc26821194032259373413
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-05-31 08:17:33 +0200

    added LikeRepositoryTsts

---
+++ Social_medie_projekt/WebApi-Tests/Repository/LikeRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi_Tests.Repository
+{
+    public class LikeRepositoryTests
+    {
+        private readonly DbContextOptions<DatabaseContext> _options;
+        private readonly DatabaseContext _context;
+        private readonly LikeRepository _likeRepository;
+
+        public LikeRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "LikeRepositoryTests")
+                .Options;
+
+            _context = new(_options);
+
+            _likeRepository = new LikeRepository(_context);
+        }
+    }
+}
