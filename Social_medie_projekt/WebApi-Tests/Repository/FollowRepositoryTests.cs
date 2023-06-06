f9d48309e619119d35b4ef3bcfe694c99eccfcde
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-06 13:10:58 +0200

    Created FollowRepositoryTests and made changes to FollowRepository

---
+++ Social_medie_projekt/WebApi-Tests/Repository/FollowRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi_Tests.Repository
+{
+    public class FollowRepositoryTests
+    {
+        private readonly DbContextOptions<DatabaseContext> _options;
+        private readonly DatabaseContext _context;
+        private readonly FollowRepository _followRepository;
+
+        public FollowRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "FollowRepositoryTests")
+                .Options;
+
+            _context = new(_options);
+
+            _followRepository = new FollowRepository(_context);
+        }
+    }
+}
--- Social_medie_projekt/WebApi/Repository/FollowRepository.cs
+++ Social_medie_projekt/WebApi/Repository/FollowRepository.cs
@@ -3,7 +3,7 @@
     public interface IFollowRepository
     {
         Task<Follow> CreateAsync(Follow newFollow);
-        Task<Follow> DeleteAsync(int userId, int followingId);
+        Task<Follow?> DeleteAsync(int userId, int followingId);
         Task<Follow?> FindByIdAsync(int userId, int followingId);
         Task<List<Follow>> FindAllByUserIdAsync(int userId);
         Task<List<Follow>> FindAllByFollowingIdAsync(int followerId);
@@ -17,22 +17,14 @@
             _context = context;
         }
 
-        // Creates a new follow relationship
         public async Task<Follow> CreateAsync(Follow newFollow)
         {
-            if (await FindByIdAsync(newFollow.UserId, newFollow.FollowingId) != null)
-            {
-                throw new Exception("User already followed");
-            }
-
             _context.Follow.Add(newFollow);
             await _context.SaveChangesAsync();
-
             return newFollow;
         }
 
-        // Deletes an existing follow relationship
-        public async Task<Follow> DeleteAsync(int userId, int followingId)
+        public async Task<Follow?> DeleteAsync(int userId, int followingId)
         {
             var follow = await FindByIdAsync(userId, followingId);
 
@@ -45,22 +37,24 @@
             return follow;
         }
 
-        // Finds a specific follow relationship
         public async Task<Follow?> FindByIdAsync(int userId, int followingId)
         {
-            return await _context.Follow.Where(x => userId == x.UserId).Where(y => followingId == y.FollowingId).FirstOrDefaultAsync();
+            return await _context.Follow
+                .FindAsync(userId, followingId);
         }
 
-        // Find all users that userId follows
         public async Task<List<Follow>> FindAllByUserIdAsync(int userId)
         {
-            return await _context.Follow.Where(x => userId == x.UserId).ToListAsync();
+            return await _context.Follow
+                .Where(x => userId == x.UserId)
+                .ToListAsync();
         }
 
-        // Find all FollowingId who follows user
         public async Task<List<Follow>> FindAllByFollowingIdAsync(int followingId)
         {
-            return await _context.Follow.Where(x => followingId == x.FollowingId).ToListAsync();
+            return await _context.Follow
+                .Where(x => followingId == x.FollowingId)
+                .ToListAsync();
         }
     }
 }
