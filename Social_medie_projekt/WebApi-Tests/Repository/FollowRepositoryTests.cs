212e9753aa2fb7a38d0e98f86adebf7c799cbf66
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-06 14:20:11 +0200

    Added more FollowRepositoryTests

--- Social_medie_projekt/WebApi-Tests/Repository/FollowRepositoryTests.cs
+++ Social_medie_projekt/WebApi-Tests/Repository/FollowRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApi_Tests.Repository
+﻿using WebApi.Database.Entities;
+
+namespace WebApi_Tests.Repository
 {
     public class FollowRepositoryTests
     {
@@ -16,5 +18,69 @@
 
             _followRepository = new FollowRepository(_context);
         }
+
+        [Fact]
+        public async void CreateAsync_ShouldFailToAddNewFollow_WhenFollowIdAlreadyExists()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            Follow follow = new()
+            {
+                UserId = 1,
+                FollowingId = 1
+            };
+
+            var result = await _followRepository.CreateAsync(follow);
+
+            // Act
+            async Task action() => await _followRepository.CreateAsync(follow);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(action);
+            Assert.Contains("An item with the same key has already been added", ex.Message);
+        }
+
+        //[Fact]
+        //public async void FindAllByUserIdAsync_ShouldReturnListOfFollows_WhenFollowsExists()
+        //{
+        //    // Arange
+        //    await _context.Database.EnsureDeletedAsync();
+
+        //    int userId = 1;
+
+        //    User user = new()
+        //    {
+        //        UserId = userId,
+        //    };
+        //    _context.User.Add(user);
+
+        //    _context.Follow.AddRange(
+        //        new Follow
+        //        {
+        //            User = user,
+        //            Following = new()
+        //            {
+        //                UserId = 2
+        //            }
+        //        },
+        //        new Follow
+        //        {
+        //            User = user,
+        //            Following = new()
+        //            {
+        //                UserId = 3
+        //            }
+        //        });
+        //    await _context.SaveChangesAsync();
+
+        //    // Act
+        //    var result = await _followRepository.FindAllByPostIdAsync(post);
+
+        //    // Assert
+        //    Assert.NotNull(result);
+        //    Assert.IsType<List<PostTag>>(result);
+        //    Assert.Equal(2, result.Count);
+        //}
     }
 }
