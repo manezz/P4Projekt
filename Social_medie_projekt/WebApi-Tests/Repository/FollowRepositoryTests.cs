37b157397774b0a1e4edbe1a2a53e2f3967775a1
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-07 12:06:50 +0200

    added more FollowRepositoryTests

--- Social_medie_projekt/WebApi-Tests/Repository/FollowRepositoryTests.cs
+++ Social_medie_projekt/WebApi-Tests/Repository/FollowRepositoryTests.cs
@@ -40,6 +40,49 @@
         }
 
         [Fact]
+        public async void FindByIdAsync_ShouldReturnFollow_WhereFollowExists()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            int userId = 1;
+            int followingUserId = 2;
+
+            _context.Follow.Add(new()
+            {
+                UserId = userId,
+                FollowingUserId = followingUserId
+            });
+
+            await _context.SaveChangesAsync();
+
+            // Act
+            var result = await _followRepository.FindByIdAsync(userId, followingUserId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Follow>(result);
+            Assert.Equal(userId, result.UserId);
+            Assert.Equal(followingUserId, result.FollowingUserId);
+        }
+
+        [Fact]
+        public async void FindByIdAsync_ShouldReturnNull_WhenFollowDoesNotExist()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            int userId = 1;
+            int followingUserId = 2;
+
+            // Act
+            var result = await _followRepository.FindByIdAsync(userId, followingUserId);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
         public async void FindAllByUserIdAsync_ShouldReturnListOfFollows_WhenFollowsExists()
         {
             // Arange
@@ -82,7 +125,7 @@
         }
 
         [Fact]
-        public async void FindAllByFollowUserIdAsync_ShouldReturnEmptyListOfFollows_WhenNoFollowsExists()
+        public async void FindAllByUserIdAsync_ShouldReturnEmptyListOfFollows_WhenNoFollowsExists()
         {
             // Arange
             await _context.Database.EnsureDeletedAsync();
@@ -138,6 +181,67 @@
             Assert.NotNull(result);
             Assert.IsType<List<Follow>>(result);
             Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async void FindAllByFollowUserIdAsync_ShouldReturnEmptyListOfFollows_WhenNoFollowsExists()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            int followingUserId = 1;
+
+            // Act
+            var result = await _followRepository.FindAllByFollowingUserIdAsync(followingUserId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<Follow>>(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async void DeleteAsync_ShouldReturnDeletedFollow_WhenFollowIsDeleted()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            int userId = 1;
+            int followingUserId = 2;
+
+            Follow follow = new()
+            {
+                UserId = userId,
+                FollowingUserId = followingUserId
+            };
+            _context.Follow.Add(follow);
+
+            await _context.SaveChangesAsync();
+
+            // Act
+            var result = await _followRepository.DeleteAsync(userId, followingUserId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Follow>(result);
+            Assert.Equal(userId, result?.UserId);
+            Assert.Equal(followingUserId, result?.FollowingUserId);
+        }
+
+        [Fact]
+        public async void DeleteAsync_ShouldReturnNull_WhenFollowDoesNotExist()
+        {
+            // Arange
+            await _context.Database.EnsureDeletedAsync();
+
+            int userId = 1;
+            int followingUserId = 2;
+
+            // Act
+            var result = await _followRepository.DeleteAsync(userId, followingUserId);
+
+            // Assert
+            Assert.Null(result);
         }
     }
 }
