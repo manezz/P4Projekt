9d9e44adb3dd45eac7017fe00d6f52a410142a54
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-07 12:22:37 +0200

    added TagServiceTests

---
+++ Social_medie_projekt/WebApi-Tests/Service/TagServiceTests.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApi_Tests.Service
+{
+    public class TagServiceTests
+    {
+        private readonly TagService _tagService;
+        private readonly Mock<ITagRepository> _tagRepositoryMock;
+        private readonly Mock<IPostTagRepository> _postTagRepositoryMock;
+
+        public TagServiceTests()
+        {
+            _tagService = new(
+                _tagRepositoryMock.Object,
+                _postTagRepositoryMock.Object);
+        }
+
+
+    }
+}
