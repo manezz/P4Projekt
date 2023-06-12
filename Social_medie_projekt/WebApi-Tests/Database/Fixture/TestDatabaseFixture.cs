678e8d759b9f0e541bcf884c21c126a1bc1f038b
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-12 14:10:24 +0200

    added database fixture

---
+++ Social_medie_projekt/WebApi-Tests/Database/Fixture/TestDatabaseFixture.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi_Tests.Database.Fixture
+{
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=(localdb)\\MSSQLLocalDB;Database=BlogDBTest;Trusted_Connection=True";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public DatabaseContext CreateContext()
+            => new DatabaseContext(
+                new DbContextOptionsBuilder<DatabaseContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+    }
+}
