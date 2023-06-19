05ab7a8fefef9e26a472d73a4510e88d3c989fba
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2023-06-19 14:06:01 +0200

    added WepApi.DAL project

---
+++ Social_medie_projekt/WebApi.DAL/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using WebApi.Database.Entities;
+global using WebApi.Database.Entities.Interfaces;
